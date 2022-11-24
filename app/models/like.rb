593426e3f50138955bd8792f42973a0fedcde481
principlebrothers <principlesbrothers@yahoo.com>
2022-11-24 21:19:32 +0000

    Set up associations between the models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,8 @@
 class Like < ApplicationRecord
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
+  belongs_to :post, counter_cache:, true
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end

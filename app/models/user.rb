d8df0dba1ba153af233d954def32de22c683b9d5
principlebrothers <principlesbrothers@yahoo.com>
2022-11-24 21:57:12 +0000

    Create custom methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
+  after_initialize :init
+  
+  def init
+    self.posts_counter ||= 0
+  end
+
   def recent_post
     posts.order(created_at: :desc).limit(3)
   end

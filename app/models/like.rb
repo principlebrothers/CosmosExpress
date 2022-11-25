e3d87b79ef98635fb8453150e6e62ee5f1f8ac17
principlebrothers <principlesbrothers@yahoo.com>
2022-11-25 01:00:40 +0000

    Update models/likes.rb with single quotation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
-  belongs_to :post, counter_cache:, true
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, counter_cache: true
 
   after_save :update_likes_counter
 

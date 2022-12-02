9d9f3634f17ed99288f0b9cf5e1d00fd4b96d65e
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-02 01:10:56 +0000

    Update posts_controller index action variable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @user_posts = @user.posts
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post = @user.posts[params[:id].to_i]
+    @post = @user.posts[params[:id].to_i - 1]
+    @post_id = @post.id
   end
 end

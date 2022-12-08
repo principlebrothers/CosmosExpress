df3bc729b2435e271136561b27acbcf8ee564605
Bakhtiyor <harhil46@gmail.com>
2022-12-08 15:52:58 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
+  # Determine where to redirect user after successful login.
+  def after_sign_in_path_for(_resource)
+    users_path
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    end
+  end
 end

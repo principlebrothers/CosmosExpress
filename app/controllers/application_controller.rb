8bcf12ef19eeebd02fd398d15d805de03788b274
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-09 08:41:53 +0000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
-  # before_action :authenticate_user!
+
+  # To redirect users to see all users after sign up
+  def after_sign_up_path_for(_resource)
+    users_path
+  end
+
   # Determine where to redirect user after successful login.
   def after_sign_in_path_for(_resource)
-    users_path
+    user_path(current_user)
   end
 
   protected
